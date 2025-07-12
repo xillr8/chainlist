9033bf5af06e0991ee41d50665e93de2a174aa4d
xillr8 <149909312+xillr8@users.noreply.github.com>
2025-07-12 16:06:24 -0400

    Create chainid-{EIP1559}.js
    My BNB Smart Chain Testnet

---
+++ constants/additionalChainRegistry/chainid-{EIP1559}.js
@@ -0,0 +1,28 @@
+{
+  "name": "BNB Smart Chain Testnet",
+  "chain": "BSC",
+  "rpc": [
+    "https://data-seed-prebsc-1-s1.binance.org:8545/",
+    "https://data-seed-prebsc-2-s1.binance.org:8545/"
+  ],
+  "faucets": [
+    "https://testnet.binance.org/faucet-smart"
+  ],
+  "nativeCurrency": {
+    "name": "Binance Coin",
+    "symbol": "tBNB",
+    "decimals": 18
+  },
+  "features": [{ "name": "EIP155" }, { "name": "EIP1559" }],
+  "infoURL": "https://testnet.bnbchain.org/",
+  "shortName": "bnbt",
+  "chainId": 97,
+  "networkId": 97,
+  "icon": "binance",
+  "explorers": [{
+    "name": "BscScan Testnet",
+    "url": "https://testnet.bscscan.com",
+    "icon": "bscscan",
+    "standard": "EIP3091"
+  }]
+}
